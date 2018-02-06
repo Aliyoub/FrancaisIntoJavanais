9d1f9c3fd4f97d0d78235c37f02c21cc7022448a
unknown <kibmimi>
2018-02-06 16:25:47 +0100

    mon premier commit

---
+++ FrancaisIntoJavanais.java
@@ -0,0 +1,54 @@
+package com.aliyou.FrancaisIntoJavanais;
+
+public class FrancaisIntoJavanais {
+	private char car;
+	private String word;
+
+	public boolean isVowel(char c) {
+		this.car = c;
+		String vowels = "aeiouy";
+
+		if (vowels.indexOf(this.car) != -1 || vowels.toUpperCase().indexOf(this.car) != -1) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isConsonant(char c) {
+		this.car = c;
+		String consonants = "bcdfghjklmnpqrstvwxz";
+
+		if (consonants.indexOf(this.car) != -1 || consonants.toUpperCase().indexOf(this.car) != -1) {
+			return true;
+		}
+		return false;
+	}
+
+	public String francaisIntoJavanais(String word) {
+		this.word = word;
+		String francaisIntoJavanais = "";		
+		int i = 0;
+		while (i < this.word.length()) {
+			// Traitement du premier caractère du mot
+			if (i==0 && isVowel(this.word.charAt(0))) {
+				francaisIntoJavanais = "av" + this.word.charAt(0);
+			}
+			// Traitement du dernier caractère du mot
+			else if (i == this.word.length() - 1) {
+				francaisIntoJavanais += this.word.charAt(i);
+			}
+
+			// Traitement du caractère s'il n'est ni le premier ni le dernier dans le mot
+			else if (isConsonant(this.word.charAt(i)) && isVowel(this.word.charAt(i + 1))) {
+				francaisIntoJavanais += this.word.charAt(i) + "av";
+			}
+			// Sinon...
+			else {
+				francaisIntoJavanais += this.word.charAt(i);
+			}
+			i++;
+		}
+		return francaisIntoJavanais;
+	}
+
+}
---
+++ FrancaisIntoJavanaisTest.java
@@ -0,0 +1,14 @@
+package com.aliyou.FrancaisIntoJavanais;
+
+public class FrancaisIntoJavanaisTest {
+
+	public static void main(String[] args) {
+		FrancaisIntoJavanais francaisIntoJavanais = new FrancaisIntoJavanais();
+		System.out.println("bonjour en Javanais :" + francaisIntoJavanais.francaisIntoJavanais("bonjour"));
+		System.out.println("chante en Javanais :" + francaisIntoJavanais.francaisIntoJavanais("chante"));
+		System.out.println("moyen en Javanais :" + francaisIntoJavanais.francaisIntoJavanais("moyen"));
+		System.out.println("exemple en Javanais :" + francaisIntoJavanais.francaisIntoJavanais("exemple"));
+		System.out.println("au en Javanais :" + francaisIntoJavanais.francaisIntoJavanais("au"));
+	}
+
+}
